5d9a5931d4d4a205537c36945ae405774db3de14
SpaceForestChu <spaceforestchu@gmail.com>
2017-05-22 11:32:58 -0400

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,42 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
+
+console.log(name + ' wants to join ' + room);
+
+jQuery('.room-title').text(room);
+
 
 socket.on('connect', function () {
   console.log('Connected to socket.io server!');
+  socket.emit('joinRoom', {
+    name: name,
+    room: room
+  });
+});
+
+socket.on('message', function (message) {
+
+  var momentTimestamp = moment.utc(message.timestamp);
+  var $message = jQuery('.messages');
+  console.log('New Message');
+  console.log(message.text);
+
+  $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+  $message.append('<p>' + message.text + '</p>');
+});
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+  event.preventDefault();
+
+  var $message = $form.find('input[name=message]');
+
+  socket.emit('message', {
+    name: name,
+    text: $message.val()
+  });
+
+  $message.val('');
 });
